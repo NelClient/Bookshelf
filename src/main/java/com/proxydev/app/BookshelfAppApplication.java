33f1944d9317f41dc9ad2de4c9d4c214a974fdc3
Nel Mendez <nelmendez@Nels-MacBook-Pro.local>
2018-09-03 07:38:09 -0600

    bookshelf test

---
+++ src/main/java/com/proxydev/app/BookshelfAppApplication.java
@@ -0,0 +1,12 @@
+package com.proxydev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshelfAppApplication.class, args);
+	}
+}
